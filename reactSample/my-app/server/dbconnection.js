aee7adc9c80c80f1a2ef292a4acff673de23cbf6
banana1019 <jyho930@gmail.com>
2022-06-01 14:43:39 +0900

    .

--- reactSample/my-app/server/dbconnection.js
+++ reactSample/my-app/server/dbconnection.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 const connection = mysql.createPool({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '1111',
-    database: 'maria_db'
-})
+  host: "localhost", // IP 주소
+  port: 3306, // PORT
+  user: "root",
+  password: "1111",
+  database: "maria_db",
+});
 
 module.exports = connection;
